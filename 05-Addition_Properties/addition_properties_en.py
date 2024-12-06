d4b5718aa62ae102e4b384a4110b58dabe56607e
SrAquaGenius <dimoisivic@gmail.com>
2024-12-06 09:14:28 +0000

    feat: Addition Properties English done

--- 05-Addition_Properties/addition_properties_en.py
+++ 05-Addition_Properties/addition_properties_en.py
@@ -2,10 +2,12 @@ from manim import *
 from manim.utils.rate_functions import ease_in_out_sine
 
 SMALL_PADD = 2
+SMALL2_PADD = 4
+MEDDIUM_PADD = 6
 BIG_PADD = 11
 
 class AdditionProperties(Scene):
-	def customPlay(self, *animations, run_time=2, rate_func=ease_in_out_sine, **kwargs):
+	def customPlay(self, *animations, run_time=1.5, rate_func=ease_in_out_sine, **kwargs):
 		self.play(*animations, run_time=run_time, rate_func=rate_func, **kwargs)
 
 
@@ -19,16 +21,16 @@ class AdditionProperties(Scene):
 		properties_group = VGroup(*properties)
 
 		self.createMenu(title, properties_group)
-		self.wait(4)
+		self.wait(2)
 		self.associativeProperty(title, properties)
 		self.bringMenuBack(title, properties_group)
-		self.wait(4)
+		self.wait(2)
 		self.commutativeProperty(title, properties)
 		self.bringMenuBack(title, properties_group)
-		self.wait(4)
+		self.wait(2)
 		self.nullElementProperty(title, properties)
 		self.bringMenuBack(title, properties_group)
-		self.wait(4)
+		self.wait(2)
 		return
 
 	# ------------------------- Associative Property --------------------------
@@ -38,7 +40,7 @@ class AdditionProperties(Scene):
 			title.animate.to_edge(UP).shift(UP * SMALL_PADD),
 			properties[0].animate.to_edge(UP),
 			properties[1].animate.to_edge(DOWN).shift(DOWN * SMALL_PADD),
-			properties[2].animate.to_edge(DOWN).shift(DOWN * SMALL_PADD)
+			properties[2].animate.to_edge(DOWN).shift(DOWN * (SMALL_PADD +0.7))
 		)
 
 		canonical = MathTex("a + (b + c)", "=", "(a + b) + c").shift(UP/2)
@@ -66,32 +68,53 @@ class AdditionProperties(Scene):
 			properties[2].animate.to_edge(DOWN).shift(DOWN * SMALL_PADD)
 		)
 
+		canonical = MathTex("a + b", "=", "b + a").shift(UP/2)
+		example = MathTex("1 + 2", "=", "2 + 1").shift(DOWN/2)
+
+		scene.customPlay(Write(canonical))
+		scene.wait(2)
+		scene.customPlay(Write(example))
 		scene.wait(4)
 
 		# Removing everything in screen
-		scene.customPlay(properties[1].animate.shift(RIGHT * BIG_PADD))
+		scene.customPlay(
+			properties[1].animate.shift(RIGHT * BIG_PADD),
+			canonical.animate.shift(RIGHT * BIG_PADD),
+			example.animate.shift(RIGHT * BIG_PADD)
+		)
 		return
 
 	# ------------------------- Null Element Property -------------------------
 	def nullElementProperty(scene, title, properties):
 		scene.customPlay(
 			title.animate.to_edge(UP).shift(UP * SMALL_PADD),
-			properties[0].animate.to_edge(UP).shift(UP * SMALL_PADD),
+			properties[0].animate.to_edge(UP).shift(UP * (SMALL_PADD + 0.7)),
 			properties[1].animate.to_edge(UP).shift(UP * SMALL_PADD),
 			properties[2].animate.to_edge(UP)
 		)
 
+		canonical = MathTex("If\;", "a + b", "=", "a", ",\;so\;", "b = 0")
+		null_element = Tex("Addition null element is 0")
+		example = MathTex("1 + 0", "=", "1")
+
+		lines = [canonical, null_element, example]
+		lines_group = VGroup(*lines)
+
+		lines_group.arrange(DOWN, buff=0.5)
+		scene.play(Write(lines_group), run_time=4)
 		scene.wait(4)
 
 		# Removing everything in screen
-		scene.customPlay(properties[2].animate.shift(RIGHT * BIG_PADD))
+		scene.customPlay(
+			properties[2].animate.shift(RIGHT * BIG_PADD),
+			lines_group.animate.shift(RIGHT * BIG_PADD)
+		)
 		return
 
 	# ---------------------------- Menu Functions -----------------------------
 	def createMenu(scene, title, properties):
 		properties.arrange(DOWN, aligned_edge=LEFT, buff=0.5)
 		scene.play(Write(title), run_time=2)
-		scene.wait(2)
 		scene.play(Write(properties), run_time=4)
 		return
 
